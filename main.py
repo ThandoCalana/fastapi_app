176804954a517933c7dd440e34af314c6ff50f31
Thando Calana <thando.calana@slipstreamdata.co.za>
2026-06-26 16:20:23 +0200

    Rebiuld using Corey Schafer Tut

--- main.py
+++ main.py
@@ -2,74 +2,69 @@ from datetime import datetime
 from random import randint
 from typing import Any
 from fastapi import FastAPI, HTTPException, Response
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
+from fastapi.responses import HTMLResponse
+# from fastapi.routing import APIRouter
 
 app = FastAPI()
+# now: datetime = datetime.now().strftime('%Y-%m-%dT%H:%M') 
 
-class Campaign(SQLModel, table=True):
-    campaign_id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    due_date: datetime | None 
-    created_at: datetime
+# app = "Aza"
+# app.add_api_route(
+#     methods=["GET"],
+#     endpoint=get_campaigns
+# )
 
-
-PG_USER = os.getenv("PG_USER")
-PG_PASSWORD = os.getenv("PG_PASSWORD")
-PH_HOST = os.getenv("PH_HOST")
-PG_DB = os.getenv("PG_DB")
-
-postgres_url = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PH_HOST}:5432/{PG_DB}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(postgres_url,connect_args=connect_args) # Used to interact with the postgres DB
-
-
-campaigns: Any = [
+campaigns: list[dict] = [
     {
         "campaign_id": 1,
-        "name": "Youth Day Special",
+        "name": "Vote for Change 2026",
         "due_date": datetime.now(),
         "created_at": datetime.now()
     },
     {
         "campaign_id": 2,
-        "name": "Black Friday Bonanza",
+        "name": "Stronger Communities Initiative",
         "due_date": datetime.now(),
         "created_at": datetime.now()
     },
     {
         "campaign_id": 3,
-        "name": "Xmas",
+        "name": "Education First Campaign",
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
+    },
+    {
+        "campaign_id": 4,
+        "name": "Clean Energy for Tomorrow",
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
+    },
+    {
+        "campaign_id": 5,
+        "name": "Healthcare Access for All",
         "due_date": datetime.now(),
         "created_at": datetime.now()
     }
 ]
 
 # GET endpoint for the homepage
-@app.get("/")
-async def get_campaigns():
-    return {"Message": "Hellow World"}
-
-@app.get("/campaigns")
-async def read_campaigns():
-    return {"Campaigns": campaigns }
+# Stacking decorators = same response on multiple routes
+@app.get("/", response_class=HTMLResponse, include_in_schema=False) # Exclude from API documentation
+@app.get("/campaigns", response_class=HTMLResponse, include_in_schema=False) # Not useful for API consumption/ dev
+def get_campaigns():
+    return f"<h1> {campaigns[0]["name"]} </h1>"
 
 
-@app.get("/campaigns/{id}")
-async def read_campaign(id: int):
+
+@app.get("/campaigns/{id}", response_class=HTMLResponse)
+def read_campaign(id: int):
     if id > (len(campaigns)):
         raise HTTPException(status_code=404)
     else:
-        return campaigns[id-1]
+        return f"<h4> {campaigns[id-1]} </h4>"
         
 @app.post("/campaigns")
-async def create_campaign(campaign: dict[str, Any]):
+def create_campaign(campaign: dict[str, Any]):
 
     new_campaign: dict[str, Any] = {
         "campaign_id": randint(100, 200),
@@ -83,7 +78,7 @@ async def create_campaign(campaign: dict[str, Any]):
 
 
 @app.put("/campaigns/{id}", status_code=201)
-async def update_campaign(id: int, body: dict[str, Any]):
+def update_campaign(id: int, body: dict[str, Any]):
 
     for index, campaign in enumerate(campaigns):
         if campaign.get("campaign_id") == id:
@@ -99,7 +94,7 @@ async def update_campaign(id: int, body: dict[str, Any]):
     raise HTTPException(status_code=404)
 
 @app.delete("/campaigns/{id}")
-async def delete_campaign(id: int):
+def delete_campaign(id: int):
 
     for index, campaign in enumerate(campaigns):
         if campaign.get("campaign_id") == id:
