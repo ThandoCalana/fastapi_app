82ab26724109381706572495cb998c553e55e0f1
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2026-07-16 14:58:03 +0200

    Converted functions to Async + HTML tweaks

--- database.py
+++ database.py
@@ -1,23 +1,28 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
 FILE_PATH = f"./campaigns.db"
 
-SQL_ALCHEMY_DATABASE_URL = f"sqlite:///{FILE_PATH}"
+SQL_ALCHEMY_DATABASE_URL = f"sqlite+aiosqlite:///{FILE_PATH}"
 
-engine = create_engine(
+engine = create_async_engine(
         SQL_ALCHEMY_DATABASE_URL, 
         connect_args={"check_same_thread":False} # Specific to SQLite, now we can use multiple threads for one db connection
         ) 
 
 # Creates a session every time we interact with backend db
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
 
 # Generator to provide a db session each time the function is called
 # Pauses execution and can be called again to resume 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+# Updated to async
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 from datetime import datetime, timedelta
 from random import randint
-from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, HTTPException, Response, Request, status, Depends
-from starlette.exceptions import HTTPException as StarletteHTTPException
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import RequestValidationError
-from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
-from sqlalchemy import select
 from typing import Annotated
-from schemas import CreateCampaign, ResponseCampaign, UpdateCampaign, CreateUser, ResponseUser, UpdateUser
+from contextlib import asynccontextmanager # Will be used as decorator for lifespan
 
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException, Request, status, Depends
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
+from fastapi.templating import Jinja2Templates
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload # For eager loading relationships in models
+
+
+from schemas import CreateCampaign, ResponseCampaign, UpdateCampaign, CreateUser, ResponseUser, UpdateUser
 from database import get_db, engine, Base
 import models
 # from fastapi.routing import APIRouter
 
-Base.metadata.create_all(bind=engine) # Looks at all models inheriting from Base and creates them if they don't exist
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+
+    # At startup
+    async with engine.begin() as conn:
+       await conn.run_sync(Base.metadata.create_all)
+    yield
+
+   # At Shtudown
+    await engine.dispose()
+# Looks at all models inheriting from Base and creates them if they don't exist
+
+app = FastAPI(lifespan=lifespan)
 
 # Mounting static folder to the app. Now, anything inside static folder is accessible
 # Inputs => file path, instance of StaticFiles class using the 'static' folder, name we can use to ref in templates
@@ -61,9 +77,15 @@ due = (now + timedelta(days=randint(1,10)))
         include_in_schema=False
         ) # Not useful for API consumption/ dev. THESE ARE ONLY HTML ENDPOINTS NOT API FUNCTIONALITY
 
-def home(request: Request, db: Annotated[Session, Depends(get_db)]):
-    query = db.execute(select(models.Campaigns))
-    campaigns = query.scalars()
+async def home(request: Request, db: Annotated[AsyncSession, Depends(get_db)]):
+    query = await db.execute(
+        select(
+            models.Campaigns
+        ).options(
+            selectinload(models.Campaigns.author)
+        )
+    )
+    campaigns = query.scalars().all()
 
     # Pass campaigns dict list to be used in/on the actual html template file
     return templates.TemplateResponse(
@@ -76,10 +98,15 @@ def home(request: Request, db: Annotated[Session, Depends(get_db)]):
 # [API] HOMEPAGE ENDPOINT
 @app.get(path="/api/campaigns", response_model=list[ResponseCampaign]) 
 
-def get_campaigns(db: Annotated[Session, Depends(get_db)]):
-    query = db.execute(select(models.Campaigns).group_by(models.Users.username))
-    campaigns = query.scalars().first() # selects one or none, Returns None
-    # campaigns = query.scalar_one() raises an error if nothing is found
+async def get_campaigns(db: Annotated[AsyncSession, Depends(get_db)]):
+    query = await db.execute(
+        select(
+            models.Campaigns
+        ).options(
+            selectinload(models.Campaigns.author) # selectinload tells the fucntion to also load relationship when it loads up the model
+        )
+    )
+    campaigns = query.scalars().all() 
 
     return campaigns
 
@@ -87,9 +114,9 @@ def get_campaigns(db: Annotated[Session, Depends(get_db)]):
 # [API] Fecth all users - Only API, why would users get access to view site users?
 @app.get(path="/api/users", response_model=list[ResponseUser]) 
 
-def get_users(db: Annotated[Session, Depends(get_db)]):
+async def get_users(db: Annotated[AsyncSession, Depends(get_db)]):
 
-    result = db.execute(select(models.Users))
+    result = await db.execute(select(models.Users))
     users = result.scalars()
 
     if users:
@@ -98,39 +125,60 @@ def get_users(db: Annotated[Session, Depends(get_db)]):
 
 
 # [API] Fecth current user's campaigns
-@app.get(path="/api/users/{user_id}/campaigns", response_model=list[ResponseUser]) 
+@app.get(path="/api/users/{user_id}/campaigns", response_model=list[ResponseCampaign]) 
 
-def user_campaigns(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    user_check = db.execute(select(models.Users).where(models.Users.user_id==user_id))
+async def user_campaigns(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    user_check = await db.execute(
+        select(
+            models.Users
+        ).where(
+            models.Users.user_id==user_id
+        )
+    )
     user_check_result = user_check.scalars().first()
     
-    if user_check_result:
-        result = db.execute(select(models.Users.campaigns).where(models.Users.user_id==user_id))
-        campaigns = result.scalars()
+    if not user_check_result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        
+    result = await db.execute(
+        select(
+            models.Campaigns
+        ).options(
+            selectinload(
+                models.Campaigns.author)
+        ).where(
+            models.Campaigns.user_id==user_id
+        )
+        )
+    campaigns = result.scalars().all()
 
-        if campaigns:
-            return campaigns
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No campaigns found")
+    if campaigns:
+        return campaigns
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No campaigns found")
     
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
 
 
 # [HTML] Fetch current user's campaigns
-@app.get(path="/users/{user_id}/campaigns", name="user_campaigns") 
+@app.get(path="/users/{user_id}/campaigns", name="user_campaigns", include_in_schema=False) 
 
-def get_user_campaigns(request: Request, user_id: int, db: Annotated[Session, Depends(get_db)]):
-    user_check = db.execute(select(models.Users).where(models.Users.user_id==user_id))
-    user_check_result = user_check.scalars().first()
+async def get_user_campaigns(request: Request, user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    user_check = await db.execute(select(models.Users).where(models.Users.user_id==user_id))
+    user = user_check.scalars().first() # selects one or none, Returns None
+    # users = query.scalar_one() raises an error if nothing is found
     
-    if user_check_result:
-        result = db.execute(select(models.Users.campaigns).where(models.Users.user_id==user_id))
-        campaigns = result.scalars()
+    if user:
+        result = await db.execute(
+            select(models.Campaigns)
+            .options(selectinload(models.Campaigns.author))
+            .where(models.Campaigns.user_id==user_id))
+        campaigns = result.scalars().all()
 
         if campaigns:
             return templates.TemplateResponse(
                     request=request,
                     name="user_campaigns.html",
-                    context={"campaigns": campaigns, "title": "User Campaigns"}
+                    context={"campaigns": campaigns, "user": user}
         )
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No campaigns found")
     
@@ -145,9 +193,9 @@ def get_user_campaigns(request: Request, user_id: int, db: Annotated[Session, De
         description="Get a campaign by its ID",
         include_in_schema=False
         )
-def campaign_page(request: Request, campaign_id: int, db: Annotated[Session, Depends(get_db)]):
+async def campaign_page(request: Request, campaign_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    results = db.execute(select(models.Campaigns).where(models.Campaigns.campaign_id==campaign_id))
+    results = await db.execute(select(models.Campaigns).options(selectinload(models.Campaigns.author)).where(models.Campaigns.campaign_id==campaign_id))
     campaigns = results.scalars()
 
     for campaign in campaigns:
@@ -174,8 +222,8 @@ def campaign_page(request: Request, campaign_id: int, db: Annotated[Session, Dep
         response_model= ResponseUser,
         status_code= status.HTTP_201_CREATED
         )
-def create_user(user: CreateUser, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(select(models.Users.user_id).where(models.Users.email==user.email))
+async def create_user(user: CreateUser, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.Users.user_id).where(models.Users.email==user.email))
     existing_user = result.scalars().first()
 
     if existing_user:
@@ -189,8 +237,9 @@ def create_user(user: CreateUser, db: Annotated[Session, Depends(get_db)]):
         email=user.email
         )
     
-    db.add(new_user)
-    db.commit()
+    db.add(new_user) # stages the new user
+    await db.commit() # commits the change (IT DOES COMETHING)
+    await db.refresh(user)
     return new_user
 
 # [API] Create new campaign
@@ -199,7 +248,7 @@ def create_user(user: CreateUser, db: Annotated[Session, Depends(get_db)]):
         response_model= ResponseCampaign,
         status_code= status.HTTP_201_CREATED
         )
-def create_campaign(campaign: CreateCampaign, db: Annotated[Session, Depends(get_db)]):
+async def create_campaign(campaign: CreateCampaign, db: Annotated[AsyncSession, Depends(get_db)]):
 
     new_campaign = models.Campaigns(
         campaign_name=campaign.campaign_name,
@@ -209,7 +258,8 @@ def create_campaign(campaign: CreateCampaign, db: Annotated[Session, Depends(get
     )
 
     db.add(new_campaign)
-    db.commit()
+    await db.commit()
+    await db.refresh(campaign, attribute_names=["author"])
 
     return new_campaign
 
@@ -220,16 +270,20 @@ def create_campaign(campaign: CreateCampaign, db: Annotated[Session, Depends(get
 # [API] PUT ENDPOINT TO UPDATE A CAMPAIGN BY ID
 @app.put(path="/api/campaigns/{campaign_id}", response_model=list[ResponseUser]) 
 
-def update_campaign_full(campaign_id: int, campaign_data: CreateCampaign, db: Annotated[Session, Depends(get_db)]):
+async def update_campaign_full(campaign_id: int, campaign_data: CreateCampaign, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    result = db.execute(select(models.Campaigns).where(models.Campaigns.campaign_id==campaign_id))
+    result = await db.execute(
+        select(models.Campaigns).options(
+            selectinload(models.Campaigns.author)
+            ).where(
+                models.Campaigns.campaign_id==campaign_id))
     campaign = result.scalars().first()
     
     if not campaign:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Campaign not found")
 
     if campaign_data.user_id != campaign.user_id:
-        user_check = db.execute(select(models.Users).where(models.Users.user_id==campaign_data.user_id))
+        user_check = await db.execute(select(models.Users).where(models.Users.user_id==campaign_data.user_id))
         user = user_check.scalars().first()
 
         if not user:
@@ -239,16 +293,18 @@ def update_campaign_full(campaign_id: int, campaign_data: CreateCampaign, db: An
     campaign.campaign_details = campaign_data.campaign_details
     campaign.user_id = campaign_data.user_id
 
-    db.commit()
-    db.refresh(campaign)
+    await db.commit()
+    await db.refresh(campaign, attribute_names=["author"])
     return campaign
 
 # [API] PATCH ENDPOINT TO UPDATE A CAMPAIGN BY ID
 @app.patch(path="/api/campaigns/{campaign_id}", response_model=ResponseCampaign) 
-def update_campaign_partial(campaign_id: int, campaign_data: UpdateCampaign, db: Annotated[Session, Depends(get_db)]):
+async def update_campaign_partial(campaign_id: int, campaign_data: UpdateCampaign, db: Annotated[AsyncSession, Depends(get_db)]):
 
     # Check campaign exists
-    result = db.execute(select(models.Campaigns).where(models.Campaigns.campaign_id==campaign_id))
+    result = await db.execute(
+        select(models.Campaigns).options(
+            selectinload(models.Campaigns.author)).where(models.Campaigns.campaign_id==campaign_id))
     campaign = result.scalars().first()
     
     if not campaign:
@@ -258,31 +314,31 @@ def update_campaign_partial(campaign_id: int, campaign_data: UpdateCampaign, db:
     for field, value in update_data.items():
         setattr(campaign, field, value)
 
-    db.commit()
-    db.refresh(campaign)
+    await db.commit()
+    await db.refresh(campaign, attribute_names=["author"]) # Load specific relationships using the attriute name
     return campaign
 
 
 # [API] PATCH ENDPOINT TO UPDATE A USER BY THEIR USER_ ID
 @app.patch(path="/api/users/{user_id}", response_model=ResponseUser) 
-def update_user_partial(user_id: int, user_data: UpdateUser, db: Annotated[Session, Depends(get_db)]):
+async def update_user_partial(user_id: int, user_data: UpdateUser, db: Annotated[AsyncSession, Depends(get_db)]):
 
     # Check user exists
-    result = db.execute(select(models.Users).where(models.Users.user_id==user_id))
+    result = await db.execute(select(models.Users).where(models.Users.user_id==user_id))
     user = result.scalars().first()
     
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No user found")
     
     if user_data.username != None and user_data.username != user.username: # Check if new username doesn't match old username
-        user_result = db.execute(select(models.Users).where(models.Users.username==user_data.username)) # Check if new username doesn't match username in DB
+        user_result = await db.execute(select(models.Users).where(models.Users.username==user_data.username)) # Check if new username doesn't match username in DB
         existing_user = user_result.scalars().first()
 
         if existing_user:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
         
     if user_data.email != None and user_data.email != user.email: 
-        email_result = db.execute(select(models.Users).where(models.Users.email==user_data.email)) 
+        email_result = await db.execute(select(models.Users).where(models.Users.email==user_data.email)) 
         existing_email = email_result.scalars().first()
 
         if existing_email:
@@ -297,8 +353,8 @@ def update_user_partial(user_id: int, user_data: UpdateUser, db: Annotated[Sessi
     # if update_data.username is not None:
     #    user.username = update_data.username
 
-    db.commit()
-    db.refresh(user)
+    await db.commit()
+    await db.refresh(user)
     return user
             
 
@@ -311,9 +367,14 @@ def update_user_partial(user_id: int, user_data: UpdateUser, db: Annotated[Sessi
         path="/api/campaigns/{campaign_id}",
         status_code=status.HTTP_204_NO_CONTENT
         )
-def delete_campaign(campaign_id: int, db: Annotated[Session, Depends(get_db)]):
+async def delete_campaign(campaign_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    results = db.execute(select(models.Campaigns).where(models.Campaigns.campaign_id==campaign_id))
+    results = await db.execute(
+        select(
+            models.Campaigns
+        ).options(
+            selectinload(models.Campaigns.author)
+            ).where(models.Campaigns.campaign_id==campaign_id))
     campaign = results.scalars().first()
 
     # Need to add checks to make sure the user making the delete is the author of the campaign
@@ -321,8 +382,8 @@ def delete_campaign(campaign_id: int, db: Annotated[Session, Depends(get_db)]):
     if not campaign:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No campaign found")
     
-    db.delete(campaign)
-    db.commit()
+    await db.delete(campaign)
+    await db.commit()
 
 
 # [API] DELETE USER BY ID
@@ -330,16 +391,16 @@ def delete_campaign(campaign_id: int, db: Annotated[Session, Depends(get_db)]):
         path="/api/users/{user_id}",
         status_code=status.HTTP_204_NO_CONTENT
         )
-def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+async def delete_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    results = db.execute(select(models.Users).where(models.Users.user_id==user_id))
+    results = await db.execute(select(models.Users).where(models.Users.user_id==user_id))
     user = results.scalars().first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No user found")
     
-    db.delete(user)
-    db.commit()
+    await db.delete(user)
+    await db.commit()
 
 
 
@@ -350,19 +411,17 @@ def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
 
 # Using Startlette Exception Handler to catch 'all' HTTP excpetions over and above fastapi's
 @app.exception_handler(StarletteHTTPException) 
-def exception_handler(request: Request, exception: StarletteHTTPException):
+async def exception_handler(request: Request, exception: StarletteHTTPException):
     
+    if request.url.path.startswith("/api/"):
+        return await http_exception_handler(request, exception)
+    
+
     message = (
                 exception.detail 
                 if exception.detail 
                 else "Error occurred. Please check your request and try again"
                 )
-    
-    if request.url.path.startswith("/api/"):
-        return JSONResponse(
-            status_code=exception.status_code,
-            content={"detail": message}
-        )
     
     return templates.TemplateResponse(
                 request, 
@@ -376,13 +435,10 @@ def exception_handler(request: Request, exception: StarletteHTTPException):
             ) 
 
 @app.exception_handler(RequestValidationError) 
-def validation_exception_handler(request: Request, exception: RequestValidationError):
+async def validation_exception_handler(request: Request, exception: RequestValidationError):
     
     if request.url.path.startswith("/api/"):
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            content={"detail": exception.errors()}
-        )
+        return await request_validation_exception_handler(request, exception)
     
     return templates.TemplateResponse(
                 request, 
